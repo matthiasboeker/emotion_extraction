5726d6094d6d4eb8a47981809b6e50ac0b3dfd80
Matthias Boeker <matthias.boeker@icloud.com>
2023-01-23 13:35:45 +0100

    remove comments and unused code

--- robustness_test.py
+++ robustness_test.py
@@ -5,14 +5,13 @@ import json
 import numpy as np
 from sklearn.metrics import matthews_corrcoef, roc_auc_score, accuracy_score
 
-from sklearn.linear_model import LogisticRegression, lasso_path
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.dummy import DummyClassifier
 
 from feature_generation.feature_generation import get_feature_df, get_train_test_split
 from feature_generation.features import (
@@ -27,7 +26,7 @@ from feature_generation.features import (
     ts_spectral_centroid,
     max_peak,
 )
-from ml_classification import feature_selection, apply_paired_t_test, apply_tailed_t_test
+from ml_classification import apply_tailed_t_test
 
 
 def load_in_pickle_file(path_to_pickles: Path, file_name: str):
@@ -37,7 +36,6 @@ def load_in_pickle_file(path_to_pickles: Path, file_name: str):
 
 def main():
     path_to_pickles = Path(__file__).parent / "data"
-    path_to_figures = Path(__file__).parent / "figures"
     match = load_in_pickle_file(path_to_pickles, "match.pkl")
     spectators = load_in_pickle_file(path_to_pickles, "spectators.pkl")
     goal_times = [goal.real_time for goal in match.goal_events]
@@ -48,7 +46,6 @@ def main():
             "27": 162000 + shift,
             "36": 216000 + shift,
         } for shift in shifts]
-    res_dummy = []
     res_bound = []
     res_bound_scores = []
     for idx, times in enumerate(control_times):
@@ -80,8 +77,7 @@ def main():
         )
         X_train = X_train.reset_index(drop=True)
         y_train = y_train.reset_index(drop=True)
-        #X_test = X_test.reset_index(drop=True)
-        #y_test = y_test.reset_index(drop=True)
+
         feature_models = {
             "log_reg": LogisticRegression(penalty="l1", solver="liblinear", random_state=0),
             "svc": SVC(C=1, kernel="rbf", random_state=0),
@@ -92,25 +88,11 @@ def main():
             "decision_forest": RandomForestClassifier(random_state=0)
 
         }
-        #selected_features = feature_selection(X_test, y_test)
-        #X_train = X_train.loc[:, selected_features]
-        #strategy = "prior"
-        #clf_dummy = DummyClassifier(strategy=strategy, random_state=0)
         metric_dict = {"accuracy_score": accuracy_score,
                        "matthews_corrcoef": matthews_corrcoef,
                        "roc_auc_score": roc_auc_score
                        }
         for metric, score in {"accuracy_score": 0.50, "matthews_corrcoef": 0.0, "roc_auc_score": 0.5}.items():
-            #res_paired_t_test, _ = apply_paired_t_test(
-            #        clf_dummy,
-            #        feature_models,
-            #        X=X_train,
-            #        y=y_train,
-            #        folds=10,
-            #        repetitions=20,
-            #        score=metric_dict[metric],
-            #    )
-            #res_dummy.append({metric: res_paired_t_test})
             res_tailed_test, scores = apply_tailed_t_test(
                     feature_models,
                     X=X_train,
@@ -123,9 +105,6 @@ def main():
             res_bound.append({metric: res_tailed_test})
             res_bound_scores.append({metric: scores})
         print(f"Iteration {idx} of {len(shifts)} is closed")
-
-    #with open('dummy_comp_test.json', 'w') as outfile:
-    #    json.dump(res_dummy, outfile)
 
     with open('lower_bound_test.json', 'w') as outfile:
         json.dump(res_bound, outfile)
--- visualise_robustness_test.py
+++ visualise_robustness_test.py
@@ -21,11 +21,7 @@ def regroup_dict_list(results):
 def regroup_dict_list_scores_mean(results):
     acc_scores = pd.DataFrame([res["accuracy_score"] for res in results if list(res.keys())[0] == "accuracy_score"])
     acc_scores = acc_scores.applymap(np.mean)
-    mcc_scores = pd.DataFrame([res["matthews_corrcoef"] for res in results if list(res.keys())[0] == "matthews_corrcoef"])
-    mcc_scores = mcc_scores.applymap(np.mean)
-    auc_scores = pd.DataFrame([res["roc_auc_score"] for res in results if list(res.keys())[0] == "roc_auc_score"])
-    auc_scores = auc_scores.applymap(np.mean)
-    return {"Accuracy": acc_scores}#{"Accuracy": acc_scores, "MCC": mcc_scores, "Area under ROC": auc_scores}
+    return {"Accuracy": acc_scores}
 
 
 def regroup_dict_list_scores_var(results):
@@ -58,40 +54,11 @@ def plot_boxplots(dfs, save_name):
     plt.savefig(Path(__file__).parent/ "figures"/ save_name)
 
 
-#def plot_boxplots(dfs, save_name):
-#    fig, axs = plt.subplots(1, 1)
-#    colors = ['lightyellow', 'lightblue', 'lightgreen', "slategrey", "purple"]
-#    for ax, metric, df in zip(axs.ravel(), dfs.keys(), dfs.values()):
-#        bplot = ax.boxplot(df, patch_artist=True, vert=True , notch=True, labels=["Log. Reg.", "SVC",
-#                                                                                  "Naive B",
-#                                                                                  "Lin Discriminant",
-#                                                                                  "KNN"])
-#        #ax.axhline(0.05, c="r")
-#        ax.tick_params(axis='x', labelrotation=90)
-#        for c, bp in zip(colors, bplot["boxes"]):
-#            bp.set_facecolor(c)
-#        #ax.set_ylabel("Accuracy")
-#        ax.set_title(metric)
-#    plt.tight_layout()
-#    plt.savefig(Path(__file__).parent/ "figures"/ save_name)
-
-
 def main():
-    path_to_lower_bound_results = Path(__file__).parent / "lower_bound_test.json"
-    path_to_dummy = Path(__file__).parent / "dummy_comp_test.json"
     path_to_scores = Path(__file__).parent / "lower_bound_scores.json"
-    json_results_lower_b = read_in_experiment_results(path_to_lower_bound_results)
-    json_results_dummy = read_in_experiment_results(path_to_dummy)
     json_results_scores = read_in_experiment_results(path_to_scores)
-    dfs_lb = regroup_dict_list(json_results_lower_b)
-    dfs_cp = regroup_dict_list(json_results_dummy)
     dfs_scores_mean = regroup_dict_list_scores_mean(json_results_scores)
-    dfs_scores_var = regroup_dict_list_scores_var(json_results_scores)
-
-    #plot_boxplots(dfs_cp, "robustness_dummy_comp.png")
-    #plot_boxplots(dfs_lb, "robustness_lower_bound.png")
-    plot_boxplots(dfs_scores_mean, "robustness_lower_bound_scores_new_new_new.png")
-    #plot_boxplots(dfs_scores_var, "robustness_lower_bound_scores_var.png")
+    plot_boxplots(dfs_scores_mean, "robustness_lower_bound_scores.png")
 
 
 if __name__ == "__main__":
